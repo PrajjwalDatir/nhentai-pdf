8ce86fab0bf4a0d7d12d34142b6100c1626b0171
Alen Yohannan <66544823+AlenSaito1@users.noreply.github.com>
2021-07-19 19:38:38 +0530

    Update oarsing methods

--- src/lib/parse.ts
+++ src/lib/parse.ts
@@ -2,8 +2,15 @@ import cheerio from 'cheerio'
 import { IDoujinInfo } from './Doujin'
 
 const parse = (text: string, link: string): IDoujinInfo => {
+    const parseText = (text: string) =>
+        text
+            .split(/[0-9]K/g)
+            .join('10')
+            .split(/\d/g)
+            .filter((tag) => tag)
     const $ = cheerio.load(text)
-    const details: Partial<IDoujinInfo['details']> = {}
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const details: any = {}
     $('.tag-container.field-name')
         .text()
         .split('\n')
@@ -12,9 +19,14 @@ const parse = (text: string, link: string): IDoujinInfo => {
         .map((tag, i, tags) => {
             if (tag.endsWith(':') && !tags[i + 1].endsWith(':'))
                 details[tag.substring(0, tag.length - 1).toLowerCase() as keyof IDoujinInfo['details']] = tags[i + 1]
-                    .replace(/(\([0-9,]+\))([a-zA-Z])/g, '$1 $2')
+                    .replace(/(\([0-9]+\))([a-zA-Z])/g, '$1 $2')
                     .split(/(?<=\))\s(?=[a-zA-Z])/)
         })
+
+    for (const key of ['parodies', 'tags', 'languages', 'characters', 'categories', 'groups', 'artists'])
+        details[key] = parseText((details[key as keyof IDoujinInfo['details']] || [''])[0])
+    details.pages = Number(details.pages[0] || 0)
+    details.uploaded = details.uploaded[0]
     return {
         title: $('#info').find('h1').text(),
         details: details as IDoujinInfo['details'],
