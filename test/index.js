e334d7b43d4be347e4ceeb87cd1da73714106e3e
Prajjwal Datir <prajjwaldatir0072000@gmail.com>
2021-07-19 21:47:55 +0530

    added README

--- test/index.js
+++ test/index.js
@@ -1,6 +1,7 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const { Doujin } = require('../dist')
 
-const doujin = new Doujin('https://nhentai.net/g/366327/')
+const doujin = new Doujin('https://nhentai.net/g/177013/')
 doujin.validate().then(console.log)
 doujin.fetch().then(console.log)
 doujin.save().then(console.log)
